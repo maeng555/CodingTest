c9a77cad334b8fa60b60c9fe50a91c98837ff661
maeng555 <mjy5188@naver.com>
2025-07-13 16:31:54 +0900

    S3 9375

---
+++ Sliver/baek9375.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t,n;
+string a,b;
+
+int main(){
+    cin >> t; // test case 
+   
+    
+   for(int i =0; i<t; i++){
+      cin >> n ;
+      map<string, int> mp;
+      for (int j = 0; j < n; j++)
+      {
+       cin >> a >> b; //stirng hat headgear
+       mp[b]++; //headgear는 증가 
+      }
+     int ret = 1;
+    for(auto k : mp){ //타입추론 하기위해 auto 이걸안쓰면 map<string,int> 선언을해야함 
+      ret *= (k.second + 1);
+    }
+    ret--; 
+
+    cout << ret << "\n";
+   }
+   
+   
+      return 0;
+    }
+    
+
+
+
