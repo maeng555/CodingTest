791d37b93a521f24d7766032472d2202fe6e3323
maeng555 <mjy5188@naver.com>
2025-07-15 00:34:44 +0900

    1940 재귀함수 combi()로도  풀기

--- Sliver/baek1940.cpp
+++ Sliver/baek1940.cpp
@@ -26,3 +26,45 @@ int main(){
    return 0;
 
 }
+// int n,m;
+// int a[15001];
+//    int cnt ;
+// //비교하는 함수 ? 재료합
+// void combi(int start, vector<int> & v){ //참조로받기
+//    //기저사례
+//    if(v.size()==2){
+//       int b = a[v[0]];
+//       int c = a[v[1]];
+//       if(b+c == m){
+//          cnt ++ ;
+//       }
+//       return;
+//    }
+//    for (int i = start+1; i < n; i++)
+//    {
+//      v.push_back(i);
+//      combi(i,v);
+//      v.pop_back();
+//    }
+   
+
+// } 
+// int main(){
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+
+   
+//    cin >> n >> m;
+   
+//    for (int i = 0; i < n; i++)
+//    {
+//       cin >> a[i];
+//    }
+
+//    vector<int> v;
+//    combi(-1,v);
+//    cout << cnt << "\n";
+//    return 0;
+   
+
+// }
