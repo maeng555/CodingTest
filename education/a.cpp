0ba077d0797d7eea5ae96687c9953de8984c592a
maeng555 <mjy5188@naver.com>
2025-07-27 03:21:58 +0900

    quad tree

--- education/a.cpp
+++ education/a.cpp
@@ -1,29 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-// ios_base::sync_with_stdio(false);
-//    cin.tie(NULL);
-//    cout.tie(NULL);
-//2와 5로 나누어 떨어지지 않는 정수 n(1 ≤ n ≤ 10000)가 주어졌을 때, 각 자릿수가 모두 1로만 이루어진 n의 배수를 찾는 프로그램을 작성하시오.
+
 typedef long long ll;
-int n ;
+int n; 
+string s;
+char a[101][101];
+
+string quard(int y, int x, int size){
+    if(size ==1) return string(1, a[y][x]); 
+    char b = a[y][x];
+    string ret = "";
+    for(int i=y; i<y+size; i++){
+        for(int j=x; j<x+size; j++){
+            if(a[i][j] != b) {
+                ret += '(';
+                ret += quard(y, x, size/2);
+                ret += quard(y, x + size/2, size/2);
+                ret += quard(y + size/2, x, size/2);
+                ret += quard(y + size/2, x + size/2, size/2);
+                ret += ')';
+                return ret;
+            }
+        }
+    }
+    return string(1, a[y][x]);
+}
 
 int main(){
-
-  while(scanf("%d",&n)!= EOF){
-    ll cnt = 1; //현재값 
-    ll ret = 1; //자릿수
-    while(true){
-     
-      if(cnt % n == 0){
-        printf("%lld\n",ret);
-        break;
-      }else{
-        cnt = (cnt*10+1)%n;
-        ret ++;
-      }
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> s; 
+        for (int j = 0; j < n; j++)
+        {
+            a[i][j] =s[j];
+         }
+        
+        
     }
-  }
-  return 0;
-  
-
+    cout << quard(0,0,n) << "\n";
+    return 0;
 }
