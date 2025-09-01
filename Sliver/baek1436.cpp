da2f41b99f4e745e8b8e1fa365cdc234a5114154
maeng555 <mjy5188@naver.com>
2025-09-01 17:13:42 +0900

    1436 cnt ++ 증가로 다시풀이

--- Sliver/baek1436.cpp
+++ Sliver/baek1436.cpp
@@ -10,3 +10,28 @@ int main() {
     }
     cout << i << "\n"; 
 }
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; 
+    cin >> n;
+
+    int cnt = 0;
+    int x = 666;
+    while (true) {
+        if (to_string(x).find("666") != string::npos) { // "666"이 들어있으면
+            cnt++;// 단독으로 쓰면 상관없음 전위 든 후위든
+            
+             if (cnt == n) {
+                cout << x << "\n";
+                break;
+            }
+        }
+        x++;
+    }
+    return 0;
+}
